be26f4830e7a60747807cdd70c3cbc408a5381c1
Houkun Zhu <houkun.zhu@getyourguide.com>
2024-12-07 14:58:34 +0100

    update terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,19 +1,7 @@
    provider "aws" {
-     region = "eu-central-1"  # Specify your desired region
-     profile = "mi4people"
+     region = var.region  # Specify your desired region
+     profile = "mi4people" # this is profile for aws, if not needed or configured, remove this line
    }
-
-   # resource "aws_ecr_repository" "covision_api_tf" {
-   #   name                 = "covision-api2"
-   #   image_tag_mutability = "MUTABLE"  # or "IMMUTABLE"
-   #   image_scanning_configuration {
-   #     scan_on_push = true
-   #   }
-   # }
-
-   # output "repository_url" {
-   #   value = aws_ecr_repository.covision_api_tf.repository_url
-   # }
 
 data "aws_caller_identity" "current" {}
 
@@ -21,21 +9,41 @@ output "aws_account_id" {
   value = data.aws_caller_identity.current.account_id
 }
 
-# Systems Manager Parameter 
-resource "aws_ssm_parameter" "covision_api_secret_tf" {
-  name  = "CoVisionAPISecretTF"
-  type  = "SecureString"
-  #todo update this to random value
-  value = "your-secret-value"  # Replace with actual secret value
+locals {
+  common_tags = {
+    project      = var.tag_project_name
+    organization = var.tag_organization_name
+  }
 }
 
-output "covision_api_secret_arn" {
-  value = aws_ssm_parameter.covision_api_secret_tf.arn
+# KMS key to encrypt and decrypt SSM Parameter
+resource "aws_kms_key" "covision_kms_key" {
+  description             = "KMS key for encrypting and decrypting CoVision API SSM Parameter secrets"
+  deletion_window_in_days = 30
+
+  # Optional: Define key usage and key spec
+  key_usage = "ENCRYPT_DECRYPT"
+  customer_master_key_spec = "SYMMETRIC_DEFAULT"
+
+  # Add tags to the KMS key
+  tags = {
+    Name        = var.kms_key_tag_name
+    project     = var.tag_project_name
+    organization = var.tag_organization_name
+  }
+}
+
+# Systems Manager Parameter 
+resource "aws_ssm_parameter" "covision_api_secret_tf" {
+  name  = var.ssm_parameter_name
+  type  = var.ssm_parameter_type
+  value = var.ssm_parameter_value
+  tags = local.common_tags
 }
 
 # lambda function role
 resource "aws_iam_role" "lambda_exec_role_tf" {
-    name = "covision_api_lambda_exec_role_tf"
+    name = var.iam_role_name
 
     assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -49,10 +57,12 @@ resource "aws_iam_role" "lambda_exec_role_tf" {
         }
     ]
     })
+
+    tags = local.common_tags
 }
 
 resource "aws_iam_policy" "lambda_ssm_policy_tf" {
-  name = "covision_api_lambda_ssm_policy_tf"
+  name = var.iam_ssm_policy_name
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -61,37 +71,55 @@ resource "aws_iam_policy" "lambda_ssm_policy_tf" {
         Effect = "Allow"
         Action = "ssm:GetParameter"
         Resource = aws_ssm_parameter.covision_api_secret_tf.arn
-      },
-      {
-        Effect = "Allow"
-        Action = "kms:Decrypt"
-        Resource = "arn:aws:kms:eu-central-1:123456789012:key/a5cc7cca-90e1-4a65-9aa6-ff5dc93bd5d6"
       }
     ]
   })
+
+  tags = local.common_tags
 }
 
+resource "aws_iam_policy" "lambda_kms_policy_tf" {
+  name = "covision_api_lambda_kms_policy_tf"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = "kms:Decrypt"
+        Resource = aws_kms_key.covision_kms_key.arn
+      }
+    ]
+  })
+
+  tags = local.common_tags
+}
 
 resource "aws_iam_policy_attachment" "lambda_basic_exec_tf" {
-    name       = "covision_api_basic_exec_tf"
+    name       = "covision_api_lambda_basic_exec_tf"
     policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
     roles      = [aws_iam_role.lambda_exec_role_tf.name]
 }
 
 resource "aws_iam_policy_attachment" "lambda_ssm_exec_tf" {
-    name       = "covision_api_ssm_exec_tf"
+    name       = "covision_api_lambda_ssm_exec_tf"
     policy_arn = aws_iam_policy.lambda_ssm_policy_tf.arn
+    roles      = [aws_iam_role.lambda_exec_role_tf.name]
+}
+
+resource "aws_iam_policy_attachment" "lambda_kms_exec_tf" {
+    name       = "covision_api_lambda_kms_exec_tf"
+    policy_arn = aws_iam_policy.lambda_kms_policy_tf.arn
     roles      = [aws_iam_role.lambda_exec_role_tf.name]
 }
 
 # Lambda function
 resource "aws_lambda_function" "covision_api_lambda_tf" {
-    function_name = "CovsionAPILambda_TF"
+    function_name = var.lambda_function_name
     role          = aws_iam_role.lambda_exec_role_tf.arn
-    package_type  = "Image"
-    # image_uri     = "181232496617.dkr.ecr.eu-central-1.amazonaws.com/covision-api:v2"
-    image_uri     = "181232496617.dkr.ecr.eu-central-1.amazonaws.com/covision-api:v3"
-    architectures = ["arm64"]
+    package_type  = var.lambda_package_type
+    image_uri     = var.lambda_image_uri
+    architectures = [var.lambda_image_architecture]
 
     # Optional: Set environment variables
     environment {
@@ -100,21 +128,25 @@ resource "aws_lambda_function" "covision_api_lambda_tf" {
       }
     }
 
-    # Optional: Memory and timeout settings
+    # Optional:  mininum Memory and timeout settings
     memory_size = 3000
     ephemeral_storage {
         size = 1000
     }
     timeout     = 180
+
+    # Add tags to the Lambda function
+    tags = local.common_tags
 }
 
 # API Gateway
 resource "aws_apigatewayv2_api" "covision_api_gateway_tf" {
-  name          = "CovisionApiGateway_TF"
-  protocol_type = "HTTP"
+  name          = var.api_gateway_name
+  protocol_type = var.api_gateway_type
   cors_configuration {
-    allow_headers = ["x-api-password"]
+    allow_headers = var.api_gateway_allow_headers
   }
+  tags = local.common_tags
 }
 
 resource "aws_lambda_permission" "apigw_lambda_tf" {
@@ -127,13 +159,13 @@ resource "aws_lambda_permission" "apigw_lambda_tf" {
 
 resource "aws_apigatewayv2_integration" "lambda_integration_tf" {
   api_id           = aws_apigatewayv2_api.covision_api_gateway_tf.id
-  integration_type = "AWS_PROXY"
+  integration_type = var.api_gateway_integration_type
   integration_uri  = aws_lambda_function.covision_api_lambda_tf.invoke_arn
 }
 
 resource "aws_apigatewayv2_route" "default_route_tf" {
   api_id    = aws_apigatewayv2_api.covision_api_gateway_tf.id
-  route_key = "POST /check-result"
+  route_key =  var.api_gateway_route_key
   target    = "integrations/${aws_apigatewayv2_integration.lambda_integration_tf.id}"
 }
 
@@ -143,8 +175,4 @@ resource "aws_apigatewayv2_stage" "default_stage_tf" {
   auto_deploy = true
 }
 
-resource "aws_apigatewayv2_stage" "prod_stage_tf" {
-  api_id      = aws_apigatewayv2_api.covision_api_gateway_tf.id
-  name        = "prod"
-  auto_deploy = false
-}
+
